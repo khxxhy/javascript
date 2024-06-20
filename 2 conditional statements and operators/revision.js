f8d3bb14536322dc9481ab0e1928331ec9424c9d
khxxhy <135722697+khxxhy@users.noreply.github.com>
2024-06-20 12:45:48 +0530

    first commit

---
+++ 2 conditional statements and operators/revision.js
@@ -0,0 +1,22 @@
+let mode = "dark";
+let color;
+if (mode === "dark") {
+  color = "black";
+} else if (mode === "pink") {
+  color = "pink";
+} else if (mode === "green") {
+  color = "green";
+} else {
+  color = "light";
+}
+
+console.log(color);
+
+
+
+
+
+let a = 5;
+let b = 2;
+console.log("a = ", a, "b = ", b);
+a = a - 1;
